4bfd62e094ef8ff40ffd3e122180c4be7183049b
Mei <meyrehay@gmail.com>
2019-04-23 23:12:41 -0700

    Tried something

---
+++ FilmFest/MovieStructTests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieStructTests.swift
+//  FilmFestTests
+//
+//  Created by Student Account on 4/22/19.
+//  Copyright © 2019 Author. All rights reserved.
+//
+
+import XCTest
+@testable import FilmFest
+class MovieStructTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
